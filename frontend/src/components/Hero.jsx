78c26325781188801591e261a106b5d1d2f8d527
Olana Teressa <oligeda16@gmail.com>
2024-11-17 14:39:37 +0300

    build the hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -12,17 +12,15 @@ const Hero = () => {
                         <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
                         <p className='font-medium text-sm md:text-base'>OUR BESTSELLER</p>
                     </div>
-                    <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arraivals</h1>
+                    <h1 className='.prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arraivals</h1>
                     <div className='flex items-center gap-2'>
                         <p className='font-semibold text-sm md:text-base'>Shop Now</p>
                         <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
                     </div>
 
                 </div>
-
-                <img className='w-full sm:w-1/2 ' src= {assets.hero_img} alt="" />
-
             </div>
+            <img className='w-full sm:w-1/2 ' src= {assets.hero_img} alt="" />
 
         </div>
     )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
         <div className='flex justify-between items-center py-5 font-medium'>
             <img src={assets.logo} alt="" className='w-36' />
 
-            <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
-                <NavLink to='/' className='flex flex-col items-center gap-1'>
+            <ul className='hidden sm:flex gap-5  text-sm text-gray-500'>
+                <NavLink to='/' className='flex flex-col items-center gap-1 '>
                     <p>HOME</p>
                     <hr className='w-2/5 border-none h-[1.5px] bg-gray-700 hidden' />
                 </NavLink>
