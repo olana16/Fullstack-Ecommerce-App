494f61b5688e73af4024fc705fd36f1c57e88ff6
Olana Teressa <oligeda16@gmail.com>
2024-11-18 16:45:40 +0300

    creating Newsletterbox and our policy

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -5,7 +5,7 @@ import PoductItem from './PoductItem'
 
 const BestSeller = () => {
 
-    const[products] =useContext(ShopContext)
+    const {products} =useContext(ShopContext)
     const[bestSeller, setBestSeller] = useState([])
 
     useEffect(()=>{
@@ -24,9 +24,9 @@ const BestSeller = () => {
 
         </div>
 
-<div className='grid grid-col-2 sm:grid-col-3 md:grid-col-4 lg:grid-col-5 gap-4 gap-y-6'>
-    {bestSeller.map((item)=>(
-        <PoductItem  id={item._id} name={item.name} image={item.image} price={item.price}/>
+<div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 '>
+    {bestSeller.map((item,index)=>(
+        <PoductItem  key={index} id={item._id} name={item.name} image={item.image} price={item.price}/>
     ))}
 
 </div>
