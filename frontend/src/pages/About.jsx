aaace4e5fb7fff2e7d0a4c1a85b26f0626833fc2
Olana Teressa <oligeda16@gmail.com>
2024-11-26 12:25:08 +0300

    updating Login, about us and contact us page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={"ABOUT"} text2={"US"} />
+
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:w-[450px]' src={assets.about_img} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab tempora soluta repudiandae quod veniam, amet vero nam blanditiis hic est itaque, ipsa sed. Corrupti magni autem sit consequuntur rerum maxime?</p>
+          <p>Electronic commerce or e-commerce is simply the buying and selling of goods and services using the internet, when shopping online.</p>
+          <b className='text-gray-800'>Our Mission</b>
+          <p>We want to empower brands and retailers to pursue a direct-to-consumer model which is seamlessly integrated with their existing B2B supply chain. To do so, we offer E-Commerce services that help our customers save time, avoid high costs and sell more regardless of the season.</p>
+        </div>
+
+      </div>
+      <div className='text-xl py-4'>
+        <Title text1={"WHY"} text2={"CHOOSE US"}/>
+      </div>
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ducimus alias quaerat labore inventore provident esse dolorem? Aliquid laborum ea quo, itaque quas.</p>
+
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convinence:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ducimus alias quaerat labore inventore provident esse dolorem? Aliquid laborum ea quo, itaque quas.</p>
+
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exeptional Customer Service:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ducimus alias quaerat labore inventore provident esse dolorem? Aliquid laborum ea quo, itaque quas.</p>
+
+        </div>
+
+      </div>
+
+      <NewsLetterBox/>
+    </div>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+
+  const[currentState, setCurrentState]= useState('Sign Up')
+
+  const onSubmitHandler= async(event)=>{
+    event.preventDefault()
+
+  }
   return (
-    <div>Login</div>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:w-96 m-auto mt-14 gap-4 text-gray-800'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='text-3xl'>{currentState}</p>
+        <hr  className='border-none font-sans h-[1.5px] w-8 bg-gray-800'/>
+
+      </div>
+      {currentState==="Login" ? " " :<input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required/>}
+      <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required/>
+      <input type="Passworp" className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required/>
+      <div className='flex justify-between w-full  text-sm mt-[-8px]'>
+        <p className='cursor-pointer'>Forgot Password ?</p>
+        {currentState==="Login" ? <p className='cursor-pointer' onClick={()=>setCurrentState("Sign Up")}>Create Account here</p>
+                                : <p className='cursor-pointer' onClick={()=>setCurrentState("Login")}> Login here</p>}
+
+      </div>
+
+      <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState==="Login" ? "Sign In" : "Sign Up"}</button>
+
+      
+    </form>
   )
 }
 
