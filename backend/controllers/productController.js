51c85c66b814a390940aab622328ea1cee616efd
Olana Teressa <oligeda16@gmail.com>
2025-07-10 21:31:12 +0300

    updating req image

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,10 +5,12 @@ const addProduct = async(req,res)=>{
     try {
 
         const{name,description,price,category,subCategory,sizes,bestseller}= req.body
-        const image1=req.files.image1 && req.files.image[1]
-        const image2=req.files.image2 && req.files.image[1]
-        const image3=req.files.image3 && req.files.image[1]
-        const image4=req.files.image4 && req.files.image[1]
+
+      const image1 = req.files.image1 && req.files.image1[0];
+const image2 = req.files.image2 && req.files.image2[0];
+const image3 = req.files.image3 && req.files.image3[0];
+const image4 = req.files.image4 && req.files.image4[0];
+
 
         const images = [image1.image2,image3,image4].filter((item)=>item!==undefined)
       let imageUrl=await Promise.all(
