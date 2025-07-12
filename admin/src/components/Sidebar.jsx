c8fde3533cf89b41fa46c2ccb8e5ac93ae00fb04
Olana Teressa <oligeda16@gmail.com>
2025-07-12 22:19:58 +0300

    sidebar menu completed

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const Sidebar = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className='w-[18%] min-h-screen border-r-2'>
+
+            <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
+
+                <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 rounded-l px-3 py-2' to='/add'>
+                <img className='w-5 h-5' src={assets.add_icon} alt="add icon" />
+                <p className='hidden md:block'>Add items</p>
+                
+                </NavLink>
+
+                <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 rounded-l px-3 py-2' to='/list'>
+                <img className='w-5 h-5' src={assets.order_icon} alt="add icon" />
+                <p className='hidden md:block'>List items</p>
+                
+                </NavLink>
+
+                <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 rounded-l px-3 py-2' to='/orders'>
+                <img className='w-5 h-5' src={assets.order_icon} alt="add icon" />
+                <p className='hidden md:block'>Orders items</p>
+                
+                </NavLink>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Sidebar
