c91ca0c54b839b081c5adf30e7c581d6d58efeaf
Olana Teressa <oligeda16@gmail.com>
2025-07-13 22:13:35 +0300

    admin login functionality created

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,24 +1,32 @@
 
 import { Route, Routes } from 'react-router-dom'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Add from './pages/Add'
 import List from './pages/List'
 import Orders from './pages/Orders'
 import Login from './components/Login'
+  import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const backEndUrl = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
 
-  const   [token, setToken] = useState("");
+  const   [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : "");
+
+  useEffect(() => {
+     localStorage.setItem('token',token);
+   
+  }, [token])
   return (
 
     <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer/>
 
       {token === ""
-        ? <Login />
+        ? <Login setToken={setToken} />
         :
 
         <>
