bac8709feffb2af5058ad85689ec20a6b443932f
Olana Teressa <oligeda16@gmail.com>
2025-07-14 20:13:21 +0300

    UI Completed for add product

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -33,32 +33,71 @@ const Add = () => {
 
       <div className='w-full'>
         <p >Product Name</p>
-        <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' />
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required />
       </div>
 
-       <div className='w-full'>
+      <div className='w-full'>
         <p >Description </p>
-        <textarea className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type Content here' />
+        <textarea className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type Content here' required />
       </div>
 
-      <div>
+      <div className='flex flex-col gap-2 sm:flex-row sm:gap-8'>
         <div>
 
-          <p>Product Category</p>
+          <p className='mb-2'>Product Category</p>
           <select className='w-full max-w-[500px] px-3 py-2'>
             <option value="Men">Men</option>
             <option value="Women">Women</option>
             <option value="Kids">Kids</option>
-            </select>
-
-            <p>Product Category</p>
+          </select>
+        </div>
+        <div>
+          <p className='mb-2'>Sub Category</p>
           <select className='w-full max-w-[500px] px-3 py-2'>
             <option value="Topwear">Topwear</option>
             <option value="Bottomwear">Bottomwear</option>
             <option value="Winter">Winter</option>
-            </select>
+          </select>
+        </div>
+
+        <div>
+          <p className='mb-2'>Price</p>
+          <input className='w-full px-3 py-2 sm:w-[120px]' type="Number" placeholder="25" />
         </div>
       </div>
+
+      <div>
+        <p className='mb-2'>Product Sizes</p>
+        <div className='flex gap-2'>
+
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+          </div>
+          
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+          </div>
+          
+          
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+          </div>
+          
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+          </div>
+          
+          <div>
+            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex gap-2 mt-2'>
+        <input type="checkbox" id='bestseller' />
+        <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
+      </div>
+      <button type='submit' className='w-28 py-2 mt-4 bg-black  text-white'>ADD</button>
     </form>
   )
 }
