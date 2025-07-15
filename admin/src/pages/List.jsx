2fc55ba67702fb0bf7bdbf43f27dbbeef8fd52da
Olana Teressa <oligeda16@gmail.com>
2025-07-15 21:07:21 +0300

    start bulding list product

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const List = () => {
+
+const [list,setList] = useState([])
+
+
+const fitchList = async () => {
+  try {
+
+    const response = await axios.get(backendUrl + '/api/product/list')
+
+    console.log(response)
+    
+  } catch (error) {
+    
+  }
+}
+
+useEffect(()=>{
+  fitchList()
+},[])
+
+
   return (
     <div>
         
