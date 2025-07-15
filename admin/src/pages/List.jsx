065a7f9a7e58a923f513ab82f920ab70892ecaed
Olana Teressa <oligeda16@gmail.com>
2025-07-15 21:33:26 +0300

    list product logic created

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,32 +1,41 @@
 import React, { use } from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
+import axios from 'axios'
+import { backEndUrl } from '../App'
+import { toast } from 'react-toastify'
 
 const List = () => {
 
-const [list,setList] = useState([])
+  const [list, setList] = useState([])
 
 
-const fitchList = async () => {
-  try {
+  const fitchList = async () => {
+    try {
 
-    const response = await axios.get(backendUrl + '/api/product/list')
+      const response = await axios.get(backEndUrl + '/api/product/')
 
-    console.log(response)
-    
-  } catch (error) {
-    
+      if (response.data.success) {
+        setList(response.data.products)
+      }
+      else {
+        toast.error(response.data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+
+    }
   }
-}
 
-useEffect(()=>{
-  fitchList()
-},[])
+  useEffect(() => {
+    fitchList()
+  }, [])
 
 
   return (
     <div>
-        
+
     </div>
   )
 }
