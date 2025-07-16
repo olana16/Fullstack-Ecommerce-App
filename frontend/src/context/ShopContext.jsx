212198a3c2f6a2cb3dfa419736d03def2e1a800b
Olana Teressa <oligeda16@gmail.com>
2025-07-16 19:22:48 +0300

    get product from backend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,89 +1,111 @@
 import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 
 export const ShopContext = createContext()
 
-const ShopContextProvider =(props)=>{
-    const currency ="$"
-    const delivery_fee= 10
-    const backEndUrl = import.meta.env.VITE_BACKEND_URL
-    const[search,setSearch] =useState('')
-    const[showSearch,setShowSearch]= useState(false)
-    const[cartItem,setCartItem] = useState({})
-    const navigate= useNavigate()
-    CONST [products, setProducts] = useState([])
+const ShopContextProvider = (props) => {
+    const currency = "$"
+    const delivery_fee = 10
+    const backEndUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3400'
+    const [search, setSearch] = useState('')
+    const [showSearch, setShowSearch] = useState(false)
+    const [cartItem, setCartItem] = useState({})
+    const navigate = useNavigate()
+    const [products, setProducts] = useState([])
 
-    const addToCart = async (itemId,size) =>{
-        let cartData= structuredClone(cartItem);
-        if(!size){
+    const addToCart = async (itemId, size) => {
+        let cartData = structuredClone(cartItem);
+        if (!size) {
             toast.error("Please selecet product size")
             return
         }
-        if(cartData[itemId]){
-            if(cartData[itemId][size]){
-                cartData[itemId][size]+=1;
-            }else{
-                cartData[itemId][size]=1
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            } else {
+                cartData[itemId][size] = 1
             }
-        }else{
-            cartData[itemId]={}
-            cartData[itemId][size]=1
+        } else {
+            cartData[itemId] = {}
+            cartData[itemId][size] = 1
         }
         setCartItem(cartData)
     }
 
-    const getCartCount =()=>{
+    const getCartCount = () => {
         let tottalCount = 0;
-        for(const items in cartItem){
-            for(const item in cartItem[items]){
+        for (const items in cartItem) {
+            for (const item in cartItem[items]) {
                 try {
-                    if(cartItem[items][item] > 0){
-                        tottalCount +=cartItem[items][item]
+                    if (cartItem[items][item] > 0) {
+                        tottalCount += cartItem[items][item]
                     }
-                    
+
                 } catch (error) {
-                    
+
                 }
             }
         }
         return tottalCount
     }
 
+    const getProducts = async () => {
+        try {
 
-const updateQuantity = async(itemId,size,quantity)=>{
-    let cartData= structuredClone(cartItem)
-    cartData[itemId][size] = quantity
+            const response = await axios.get(backEndUrl + '/api/product/')
 
-    setCartItem(cartData)
-
-
-
+if (response.data.success) {
+    setProducts(response.data.products)
+}else{
+    toast.error(response.data.message)
 }
 
-const getCartAmount = ()=>{
-    let totalAmount= 0;
-    for(const items in cartItem){
-        let itemInfo = products.find((product)=> product._id===items)
-        for(const item in cartItem[items]){
-            try {
-                if(cartItem[items][item]>0){
-                    totalAmount+=itemInfo.price * cartItem[items][item]
-                }
-            } catch (error) {
-                
-            }
+        } catch (error) {
+toast.error(error.message)
         }
     }
-    return totalAmount
-}
 
-    const value= {
 
-        products, currency,delivery_fee,search,
-        setSearch,showSearch,setShowSearch,cartItem,addToCart,
-        getCartCount,updateQuantity,getCartAmount,navigate
+    useEffect(() => {
+        getProducts()
+    }, [])
+
+
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItem)
+        cartData[itemId][size] = quantity
+
+        setCartItem(cartData)
+
+
+
+    }
+
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const items in cartItem) {
+            let itemInfo = products.find((product) => product._id === items)
+            for (const item in cartItem[items]) {
+                try {
+                    if (cartItem[items][item] > 0) {
+                        totalAmount += itemInfo.price * cartItem[items][item]
+                    }
+                } catch (error) {
+
+                }
+            }
+        }
+        return totalAmount
+    }
+
+    const value = {
+
+        products, currency, delivery_fee, search,
+        setSearch, showSearch, setShowSearch, cartItem, addToCart,
+        getCartCount, updateQuantity, getCartAmount, navigate
 
     }
 
