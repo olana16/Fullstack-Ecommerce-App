287cdf927b782144c4df2f0acc2595072f8563e9
Olana Teressa <oligeda16@gmail.com>
2025-07-16 20:29:56 +0300

    login for front end completed

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -15,6 +15,7 @@ const ShopContextProvider = (props) => {
     const [cartItem, setCartItem] = useState({})
     const navigate = useNavigate()
     const [products, setProducts] = useState([])
+    const [token, setToken] = useState('')
 
     const addToCart = async (itemId, size) => {
         let cartData = structuredClone(cartItem);
@@ -101,11 +102,18 @@ toast.error(error.message)
         return totalAmount
     }
 
+
+    useEffect(()=>{
+        if(!token && localStorage.getItem('token')){
+            setToken(localStorage.getItem('token'))
+        }
+    })
+
     const value = {
 
         products, currency, delivery_fee, search,
         setSearch, showSearch, setShowSearch, cartItem, addToCart,
-        getCartCount, updateQuantity, getCartAmount, navigate
+        getCartCount, updateQuantity, getCartAmount, navigate,backEndUrl,token,setToken
 
     }
 
