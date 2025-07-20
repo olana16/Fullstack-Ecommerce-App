8dbacc7bc20e802b9b412d32fbcf3bf6fb022c6d
Olana Teressa <oligeda16@gmail.com>
2025-07-20 13:30:42 +0300

    update navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
                         <div className='group-hover:block hidden absolute dropdowm-menu right-0 pt-4'>
                             <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                                 <p className='cursor-pointer hover:text-black'>My Profie</p>
-                                <p className='cursor-pointer hover:text-black'>Orders</p>
+                                <p onClick={()=>navigate('/orders')} className='cursor-pointer hover:text-black'>Orders</p>
                                 <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
 
                             </div>
