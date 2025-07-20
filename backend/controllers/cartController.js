b964df983afead29f30867fd19235ff81d053a97
Olana Teressa <oligeda16@gmail.com>
2025-07-20 13:41:08 +0300

    start creating cart controller and route

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,25 @@
+  // add product to user's cart
+
+  const addToCart = async (req, res) =>{
+
+
+  }
+
+
+  // update cart
+
+  const updateCart = async (req, res) =>{
+
+    
+  }
+
+
+  // get  user's cart
+
+  const getCart = async (req, res) =>{
+
+    
+  }
+
+
+  export {addToCart, updateCart, getCart};
