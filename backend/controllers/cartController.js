285d154c13bbf5ca194a6789c8fa6512573841f2
Olana Teressa <oligeda16@gmail.com>
2025-07-21 19:42:26 +0300

    add to cart created

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,25 +1,50 @@
-  // add product to user's cart
+// add product to user's cart
 
-  const addToCart = async (req, res) =>{
+const addToCart = async (req, res) => {
+
+  try {
+
+    const { userId, itemId, size } = req.body;
+    const userData = await userModel.findById(userId);
+    let cartData = userData.cartData
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1
+      }
+
+    } else {
+      cartData[itemId] = {}
+      cartData[itemId][size] = 1
+    }
+    await userModel.findByIdAndUpdate(userId, { cartData: cartData }, { new: true });
+    res.json({ success: true, message: "item added to cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "error adding item to cart" });
 
 
   }
 
 
-  // update cart
-
-  const updateCart = async (req, res) =>{
-
-    
-  }
+}
 
 
-  // get  user's cart
+// update cart
 
-  const getCart = async (req, res) =>{
-
-    
-  }
+const updateCart = async (req, res) => {
 
 
-  export {addToCart, updateCart, getCart};
+}
+
+
+// get  user's cart
+
+const getCart = async (req, res) => {
+
+
+}
+
+
+export { addToCart, updateCart, getCart };
