05e99be68c4ad0c82006401cf65f99cba888d38d
Olana Teressa <oligeda16@gmail.com>
2025-07-22 20:17:42 +0300

    start creating order controllers

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,45 @@
+
+
+// place order using cod method
+const placeOrder = async (req, res) => {
+
+
+}
+
+
+// place order using stripe method
+const placeOrderStripe = async (req, res) => {
+
+
+}
+
+// place order using Razorpay method
+const placeOrderRazorpay = async (req, res) => {
+
+
+}
+
+
+// All orders data for dmin panel
+const allOrders = async (req, res) => {
+
+
+}
+
+// Users order data for front end
+const userOrders = async (req, res) => {
+
+
+}
+
+
+// update order status from admin pannel
+
+const updateOrderStatus = async (req, res) => {
+
+
+}
+
+export {
+    placeOrder, placeOrderRazorpay, placeOrderStripe, allOrders, userOrders, updateOrderStatus
+}
