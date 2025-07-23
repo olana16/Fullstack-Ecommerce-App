f5a089b73875ac9aecdfc6748515d0b1a975b589
Olana Teressa <oligeda16@gmail.com>
2025-07-23 19:58:26 +0300

    backend update

---
+++ backend/route/oredrRouter.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { placeOrder, placeOrderRazorpay,placeOrderStripe,allOrders,userOrders,updateOrderStatus } from "../controllers/orderController.js"
+import adminAuth from '../middleware/adminAuth.js'
+import authUser from '../middleware/auth.js'
+const orderRouter = express.Router()
+
+// Admin features
+orderRouter.post("/list",adminAuth, allOrders);
+orderRouter.post("/update", adminAuth, updateOrderStatus);
+
+// payment features
+orderRouter.post("/place", placeOrder);
+orderRouter.post("/razorpay", placeOrderRazorpay);
+orderRouter.post("/stripe", placeOrderStripe);
+
+// userfeatures
+orderRouter.post("/userorders",authUser,userOrders)
+
+export default orderRouter
