b0143cc29e8b2b1572cf5ef88963468c9f0bb033
Olana Teressa <oligeda16@gmail.com>
2025-07-26 11:04:53 +0300

    update placeorder,ordercontrollers and ordermodel

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,5 @@
 import userModel from "../models/userModel.js";
-
+import orderModel from "../models/orderModel.js";
 
 // place order using cod method
 const placeOrder = async (req, res) => {
--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -5,7 +5,7 @@ const orderSchema = new mongoose.Schema({
     userId:{type:String, required:true},
     item:{type:Array, required:true},
     amount:{type:Number, required:true},
-    address:{type:object, required:true},
+    address:{type:Object, required:true},
     status:{type:String, required:true, default:"order placed"},
     paymentMethod:{type:String, required:true},
     payment:{type:Boolean, required:true, default:false},
