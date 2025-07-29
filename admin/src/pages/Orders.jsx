e87a462fec9e25cae9e1d1c4052035f3a4e4e8da
Olana <oligeda16@gmail.com>
2025-07-29 07:49:57 -0400

    updated css

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import backEndUrl from '../App'
-import toast from 'react-toastify'
+import backEndUrl, { currency } from '../App'
+import {toast} from 'react-toastify'
 import { assets } from '../assets/assets'
 
 const Orders = (token) => {
@@ -51,31 +51,52 @@ const Orders = (token) => {
           orders.map((order, index) => (
             <div key={index}>
               <img src={assets.parcel_icon} alt="" />
-              <div>
-                {
-                  order.items.map((item, index) => {
-
-                    if (index === order.items.length - 1) {
-                      return <p key={index}>{item.name} x {item.quantity} <span>{item.size}</span></p>
-
-                    }
-                    else {
-                      return <p key={index}>{item.name} x {item.quantity} <span>{item.size}</span>,</p>
-
-                    }
-
-                  })
-                }
-              </div>
-              <p>{order.address.firstName + " " + order.address.lastName}</p>
 
               <div>
-                <p>{order.address.street + " ,"}</p>
-                <p>{order.address.city + " ," + order.address.state + ", " + order.address.country + ", " + order.address.zipcode}</p>
+                <div>
+                  {
+                    order.items.map((item, index) => {
+
+                      if (index === order.items.length - 1) {
+                        return <p key={index}>{item.name} x {item.quantity} <span>{item.size}</span></p>
+
+                      }
+                      else {
+                        return <p key={index}>{item.name} x {item.quantity} <span>{item.size}</span>,</p>
+
+                      }
+
+                    })
+                  }
+                </div>
+                <p>{order.address.firstName + " " + order.address.lastName}</p>
+
+                <div>
+                  <p>{order.address.street + " ,"}</p>
+                  <p>{order.address.city + " ," + order.address.state + ", " + order.address.country + ", " + order.address.zipcode}</p>
+
+                </div>
+                <p>{order.address.phone}</p>
 
               </div>
-              <p>{order.address.phone}</p>
 
+
+
+              <div>
+                <p>Items: {order.items.length}</p>
+                <p>Method: {order.paymentMethod}</p>
+                <p>Payment: {order.payment ? " Done " : "Pending"}</p>
+                <p>Date: {new Date(order.date).toLocaleDateString()}</p>
+              </div>
+              <p>{currency} {order.amount}</p>
+              <select>
+                <option value="Order Placed">Order Placed</option>
+                <option value="Packing">Packing</option>
+                <option value="Shipped">Shipped</option>
+                <option value="Out For Delivery">Out For Delivery</option>
+                <option value="Delivered">Delivered</option>
+
+              </select>
             </div>
           ))
         }
