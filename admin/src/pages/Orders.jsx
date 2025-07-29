26085f84b557fabb4907572850b71a57a69bb7f8
Olana <oligeda16@gmail.com>
2025-07-29 05:04:26 -0400

    working on orders ui

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,41 +1,80 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import backEndUrl from '../App'
+import toast from 'react-toastify'
+import { assets } from '../assets/assets'
 
 const Orders = (token) => {
 
-  const [orders, setOrders] = useState( [])
+  const [orders, setOrders] = useState([])
 
 
-  const fetchAllOrders = async () =>{
+  const fetchAllOrders = async () => {
 
-    if(!token){
+    if (!token) {
       return null
     }
 
     try {
 
-      const response = await axios.post(backEndUrl + '/api/order/list',{},{headers:{token}})
-      
-      
+      const response = await axios.post(backEndUrl + '/api/order/list', {}, { headers: { token } })
+
+      if (response.data.success) {
+        setOrders(response.data.orders)
+      } else {
+        toast.error(response.data.message)
+      }
+
+
     } catch (error) {
-      
+
     }
 
 
   }
 
 
-  useEffect(()=>{
+  useEffect(() => {
 
     fetchAllOrders()
 
-  },[token])
+  }, [token])
 
 
   return (
     <div>
-        
+
+      <h3>Orders page</h3>
+      <div>
+
+        {
+          orders.map((order, index) => (
+            <div key={index}>
+              <img src={assets.parcel_icon} alt="" />
+              <div>
+                {
+                  order.items.map((item, index) => {
+
+                    if (index === order.items.length - 1) {
+                      return <p key={index}>{item.name} x {item.quantity} <span>{item.size}</span></p>
+
+                    }
+                    else {
+                      return <p key={index}>{item.name} x {item.quantity} <span>{item.size}</span>,</p>
+
+                    }
+
+                  })
+                }
+              </div>
+
+
+            </div>
+          ))
+        }
+
+      </div>
+
     </div>
   )
 }
