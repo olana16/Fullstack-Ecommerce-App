4528cfd6bf922c86e59bbed00796643ee73362ee
Olana <oligeda16@gmail.com>
2025-08-02 09:35:50 -0400

    verification for stripe completed

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { useSearchParams } from 'react-router-dom'
+import {toast} from 'react-toastify'
+import { useEffect } from 'react'
+import axios from 'axios'
+
+const { navigate, token, setCartItem, backEndUrl } = useContext(ShopContext)
+const [searchParams, setSearchParams] = useSearchParams()
+
+const success = searchParams.get('sucess')
+const orderId = searchParams.get('orderId')
+const verifyPayment = async() => {
+
+    try {
+
+        if (!token) {
+            return null
+        }
+
+        const response = await axios.post(backEndUrl + '/api/order/verifyStripe',{success,orderId},  {headers:{token}})
+        if(response.data.success){
+            setCartItem({})
+            navigate('/orders')
+        }else{
+            navigate('/cart')
+        }
+
+    } catch (error) {
+        console.log(error)
+        toast.error(error.message)
+
+    }
+
+}
+
+useEffect(() => {
+
+    verifyPayment()
+
+}, [token])
+
 
 const Verify = () => {
-  return (
-    <div>Verify</div>
-  )
+    return (
+        <div>
+
+        </div>
+    )
 }
 
 export default Verify
