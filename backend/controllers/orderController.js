ed72bcb7faa23da4e435eb5f94ecf818d922d0c0
Olana <oligeda16@gmail.com>
2025-08-02 08:50:05 -0400

    create strpe payment verify controllers

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -115,6 +115,33 @@ const placeOrderStripe = async (req, res) => {
 
 }
 
+
+//verify stripe 
+
+const verifyStripe = async(req,res)=>{
+    const{orderId,success,userId} = req.body;
+
+    try {
+        
+if(success == "true"){
+    await orderModel.findByIdAndUpdate(orderId,{payment:true})
+    await userModel.findByIdAndUpdate(userId,{cartData:{}})
+    res.json({success:true})
+
+}else{
+    await orderModel.findByIdAndDelete(orderId)
+    res.json({success:false})
+}
+
+    } catch (error) {
+        
+
+        console.log(error)
+        res.json({ success: false, message: error.message })
+
+    }
+}
+
 // place order using Razorpay method
 const placeOrderRazorpay = async (req, res) => {
 
